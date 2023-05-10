099961079fb27bf5bc7c65e576f08e5a4fcbe134
Ilias Karamanis <karamanisweb@gmail.com>
2023-05-10 22:22:28 +0200

    Introduce commit linting.

---
+++ commitlint.config.js
@@ -0,0 +1,16 @@
+'use strict'
+
+module.exports = {
+  rules: {
+    'body-leading-blank': [2, 'always'],
+    'body-max-line-length': [2, 'always', 72],
+    'body-min-length': [2, 'always', 0],
+
+    'footer-max-line-length': [2, 'always', 72],
+
+    'header-case': [2, 'always', 'sentence-case'],
+    'header-max-length': [2, 'always', 50],
+
+    'subject-case': [2, 'always', ['sentence-case']]
+  }
+};
