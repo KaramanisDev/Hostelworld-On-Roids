7540cc761e014193327d5c4a0bada29289181ba6
Ilias Karamanis <karamanisweb@gmail.com>
2026-03-11 20:20:32 +0100

    Remove redundant void operator.

--- src/app/Listeners/WorkerTaskDispatchListener.ts
+++ src/app/Listeners/WorkerTaskDispatchListener.ts
@@ -5,6 +5,6 @@ import { WorkerRPCProxy } from 'Communication/WorkerRPCProxy'
 @Subscribe('worker:task:dispatch')
 export class WorkerTaskDispatchListener extends AbstractListener {
   public handle (task: string, ...args: unknown[]): void {
-    void WorkerRPCProxy.call(task, args)
+    WorkerRPCProxy.call(task, args)
   }
 }
