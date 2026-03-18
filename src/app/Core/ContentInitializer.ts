92f5b29fa7a458b43e35c95d685798e588163063
Ilias Karamanis <karamanisweb@gmail.com>
2026-03-18 21:39:54 +0100

    Load app script earlier in page context.

--- src/app/Core/ContentInitializer.ts
+++ src/app/Core/ContentInitializer.ts
@@ -1,4 +1,3 @@
-import { ScriptLoader } from 'Utils/ScriptLoader'
 import { ExtensionRuntime } from 'Utils/ExtensionRuntime'
 import { ExtensionConfig } from 'Utils/ExtensionConfig'
 import { WorkerRPCEndpoint } from 'Communication/WorkerRPCEndpoint'
@@ -16,17 +15,6 @@ export class ContentInitializer {
           icon: ExtensionRuntime.assetUrl('icons/icon32.png')
         }
       }
-    )
-
-    /*
-     Why? you wonder?
-     Well in order to have access to the window attributes to be able to do things like intercepting XHR requests etc...
-     You'll need your script to run in the page itself, and not within the content scripts of the extension.
-
-     Additional benefits it offers is that we won't have to reload the extension in the browser on every build.
-    */
-    return ScriptLoader.inject(
-      ExtensionRuntime.assetUrl('roids.js')
     )
   }
 }
